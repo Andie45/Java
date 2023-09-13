2b86fe2ee711afba85c38ba3acb7b71b34cb7bc0
Andie45 <123144842+Andie45@users.noreply.github.com>
2023-09-13 17:25:52 -0400

    Update test.java

--- test.java
+++ test.java
@@ -4,7 +4,5 @@ import java.util.Scanner
     public static void main(String[] args)
     {
       System.out.print("Hello");
-      Scanner keyboard = new Scanner(System.in);
-      System.out.print("What is your name? ");
-      Scanner.nextInt();
+
     }
