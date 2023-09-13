17055f278ab439bff6f48752dc6457624a572f03
Andie45 <123144842+Andie45@users.noreply.github.com>
2023-09-13 17:24:12 -0400

    Create test.java

---
+++ test.java
@@ -0,0 +1,10 @@
+import java.util.Scanner
+
+  public class test
+    public static void main(String[] args)
+    {
+      System.out.print("Hello");
+      Scanner keyboard = new Scanner(System.in);
+      System.out.print("What is your name? ");
+      Scanner.nextInt();
+    }
